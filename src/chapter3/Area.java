9376b6aa53cc0c3b3b3a3eec626b99fe8cd45d22
Ilya <iljaust7@gmail.com>
2020-04-26 14:38:42 +0300

    Adding chapter 3

---
+++ src/chapter3/Area.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+public class Area {
+    public static void main(String[] args) {
+        double pi, a, r;
+
+        r = 10.8;
+        pi = 3.1416;
+        a = r * pi;
+
+        System.out.println("Плoщaдь круга равна " + a);
+    }
+}
